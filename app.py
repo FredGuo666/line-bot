60869232bb12832f5d086e1f334aa2acfa946cb3
Fred <fredguo@Fred-Mac.local>
2022-01-21 14:41:43 +0800

    update

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def handle_message(event):
 
         line_bot_api.reply_message(
         event.reply_token,
-        StickerSendMessage)
+        sticker_message)
         return
 
     if msg in ['hi','Hi']:
